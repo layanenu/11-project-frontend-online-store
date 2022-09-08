0aeabf65dc24f9e510c9baa72fcc3a5354c1a879
caren <carenoliveirasilvapontes@gmail.com>
2022-09-08 17:35:21 -0500

    iniciando 3

--- src/pages/TelaPrincipal.jsx
+++ src/pages/TelaPrincipal.jsx
@@ -11,20 +11,14 @@ class TelaPrincipal extends React.Component {
     return (
       <div>
         <label htmlFor="procura">
-          <input
-            type="text"
-            name="procura"
-          />
+          <input type="text" name="procura" />
         </label>
         {listProduct.length <= 0 && (
-
           <p data-testid="home-initial-message">
             Digite algum termo de pesquisa ou escolha uma categoria.
           </p>
-
         )}
       </div>
-      
     );
   }
 }
