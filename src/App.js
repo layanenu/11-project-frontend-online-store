a9f6326d27df218b544947f5c70ae29b754144cc
Layane Nunes <layane@MacBook-Air-de-Layane.local>
2022-09-09 18:52:45 -0300

    atualizando local

--- src/App.js
+++ src/App.js
@@ -1,17 +1,9 @@
 import React, { Component } from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import TelaPrincipal from './pages/TelaPrincipal';
-
 import './App.css';
-import * as api from './services/api';
 
 class App extends Component {
-  componentDidMount() {
-    api.getCategories().then((categories) => { console.log(categories); });
-    api.getProductsFromCategoryAndQuery()
-      .then((categoria) => { console.log(categoria); });
-  }
-
   render() {
     return (
       <BrowserRouter>
@@ -19,7 +11,6 @@ class App extends Component {
           <Route exact path="/" component={ TelaPrincipal } />
         </Switch>
       </BrowserRouter>
-
     );
   }
 }
